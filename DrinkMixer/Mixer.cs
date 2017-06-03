49dea60e3a271dad4074840c5c348415c6adbff5
Eugene Morgunov <evmorgunov@yahoo.com>
2017-06-03 22:41:02 +0300

    Add project files.

---
+++ DrinkMixer/Mixer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkMixer
+{
+    class Mixer
+    {
+        public int Mix(int Drink, List<int> Additions)
+        {
+            int cost = 1;
+            return cost;
+        }
+    }
+}
---
+++ DrinkMixer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkMixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите напиток");
+            int DrinkID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите добавки");
+            List<int> AdditionIDs = (Console.ReadLine().Split(',')).Select(int.Parse).ToList();
+
+            Console.WriteLine("Стоимость:");
+        }
+    } 
+}
